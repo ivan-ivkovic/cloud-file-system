e8db7f391552865885a4399d30ad064977d389f1
Ivan Ivković <ivkovic.ivan@outlook.com>
2022-05-07 23:21:32 +0200

    Removed unnecessary status code property.

--- Source/FileSystem/Files/FileRepositoryException.cs
+++ Source/FileSystem/Files/FileRepositoryException.cs
@@ -6,14 +6,10 @@ public class FileRepositoryException : Exception
 {
     public FileRepositoryException(string message, int statusCode = 400) : base(message)
     {
-        this.StatusCode = statusCode;
     }
 
     public FileRepositoryException(string message, Exception e, int statusCode = 400)
         : base(message, e)
     {
-        this.StatusCode = statusCode;
     }
-
-    public int StatusCode { get; set; }
 }
