85ea3f91c65218d44f96057693495c971e6eaee5
Ivan Ivković <ivkovic.ivan@outlook.com>
2022-05-09 08:23:21 +0200

    Checking whether a folder exists before creating a file.

--- Source/FileSystem/Files/FileCreator.cs
+++ Source/FileSystem/Files/FileCreator.cs
@@ -1,18 +1,26 @@
 using P3Mobility.CloudFileSystem.FileSystem.Exceptions;
+using P3Mobility.CloudFileSystem.FileSystem.Folders;
 
 namespace P3Mobility.CloudFileSystem.FileSystem.Files;
 
 public class FileCreator
 {
     private readonly IFileRepository fileRepository;
+    private readonly IFolderRepository folderRepository;
 
-    public FileCreator(IFileRepository fileRepository)
+    public FileCreator(IFileRepository fileRepository, IFolderRepository folderRepository)
     {
         this.fileRepository = fileRepository;
+        this.folderRepository = folderRepository;
     }
 
     public FileModel CreateFile(CreateOrUpdateFileModel file)
     {
+        if (!this.folderRepository.FolderExists(file.FolderId))
+        {
+            throw new FileSystemException("Folder does not exist.");
+        }
+
         if (this.fileRepository.FileExists(file.FolderId, file.Name))
         {
             throw new FileSystemException("File already exists.");
