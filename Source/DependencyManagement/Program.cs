d1105d01b19208f5c7c6dcee9aa28317d18fd38e
Ivan Ivković <ivkovic.ivan@outlook.com>
2022-05-09 08:47:15 +0200

    Removed unnecessary code.

--- Source/DependencyManagement/Program.cs
+++ Source/DependencyManagement/Program.cs
@@ -14,31 +14,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-// using (var scope = app.Services.CreateScope())
-// {
-//     var dbContext = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
-//     if (dbContext.Folders == null || dbContext.Hierarchies == null)
-//     {
-//         throw new Exception();
-//     }
-
-//     var rootFolderId = Guid.NewGuid();
-//     dbContext.Folders.Add(new FolderModel
-//     {
-//         Id = rootFolderId,
-//         FolderName = "/",
-//         ParentFolderId = Guid.Empty
-//     });
-//     dbContext.Hierarchies.Add(new HierarchyModel
-//     {
-//         Id = Guid.NewGuid(),
-//         ParentFolderId = rootFolderId,
-//         ChildFolderId = rootFolderId,
-//         Depth = 0
-//     });
-//     dbContext.SaveChanges();
-// }
-
 app.MapControllers();
 
 app.Run();
